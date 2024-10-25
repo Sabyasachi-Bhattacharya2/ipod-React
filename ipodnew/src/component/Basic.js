d111cee441f1c9db1b1e3610f8ea5cae913d2c63
Sabyasachi Bhattacharya <bhattacharyasabyasachi2@gmail.com>
2024-10-25 16:29:42 +0530

    preparing design of the UI

--- ipodnew/src/component/Basic.js
+++ ipodnew/src/component/Basic.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
 
+import "./style.css"
+
 export default class Basic extends Component {
 
 
@@ -11,15 +13,17 @@ export default class Basic extends Component {
 
     render() {
         return(
-            <div>
-                <div>
-                    Card Div
-                </div>
-                <div className="circular">
-                    <button>Menu</button>
-                    <button>Up</button>
-                    <button>Down</button>
-                    <button>Play/Pause</button>
+            <div className="container">
+                <div className="ipod">
+                    <div className="card">
+                        Card Div
+                    </div>
+                    <div className="circular">
+                        <button>Menu</button>
+                        <button>Up</button>
+                        <button>Down</button>
+                        <button>Play/Pause</button>
+                    </div>
                 </div>
             </div>
         )
