84effdb2f2c294edef65cf2068cc1969b5d6f465
Sabyasachi Bhattacharya <bhattacharyasabyasachi2@gmail.com>
2024-10-26 10:53:30 +0530

    modified now

--- ipodnew/src/component/Basic.js
+++ ipodnew/src/component/Basic.js
@@ -19,10 +19,11 @@ export default class Basic extends Component {
                         Card Div
                     </div>
                     <div className="circular">
-                        <button>Menu</button>
-                        <button>Up</button>
-                        <button>Down</button>
-                        <button>Play/Pause</button>
+                        <button><i className="fas fa-bars"></i></button>
+                        <button><i className="fas fa-chevron-up"></i></button>
+                        <button><i className="fas fa-chevron-down"></i></button>
+                        <button><i className="fas fa-play"></i>/<i className="fas fa-pause"></i></button>
+                        <button><i className="fas fa-dot-circle"></i></button>
                     </div>
                 </div>
             </div>
